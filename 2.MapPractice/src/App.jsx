6990e5de37ea4541badea941f22fc407a9e39851
RupeshKotal <rupeshkotal99@gmail.com>
2024-01-05 01:01:36 +0530

    learn childer props

--- 2.MapPractice/src/App.jsx
+++ 2.MapPractice/src/App.jsx
@@ -3,17 +3,18 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Allitem from "./components/Allitem";
 import Error from "./components/Error";
+import Container from "./components/container";
 
 function App() {
   // let foodItem= []
  let foodItem = ["Dal","rice","vegetable","Roti","Salad"]
 
   return (
-    <>
+   <Container>
     <h1>Healty Food</h1>
        <Error item = {foodItem}></Error>
       <Allitem menu={foodItem}></Allitem>
-    </>
+      </Container>
   );
 }
 
