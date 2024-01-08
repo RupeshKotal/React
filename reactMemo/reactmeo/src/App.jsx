8a8729cef2f9fafe613952ec00341f09d93d5d29
RupeshKotal <rupeshkotal99@gmail.com>
2024-01-09 01:40:52 +0530

    learning useEffec

--- reactMemo/reactmeo/src/App.jsx
+++ reactMemo/reactmeo/src/App.jsx
@@ -1,57 +1,40 @@
-import { useState } from "react";
-
-function App() {
-  let counter = 4;
-
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      title:"go to gym",
-      description:"go to gym at 9 to 10"
-    },{
-      id:2,
-      title:"go to class",
-      description:"go to gym at 9 to 10"
-    },{
-      id:3,
-      title:"go to ofc",
-      description:"go to gym at 9 to 10"
-    }
-  ]);
+import { useEffect, useState } from "react";
 
 
-  function addTodo() {
-    setTodos([...todos,{
-      id:counter++,
-      title:"go to mall",
-      description:Math.random()
-    }])
-  }
+function App(){
 
-  return (
-    <div>
- 
-   <button onClick={addTodo}>Add a todo</button>
+  const [todos,setTodos] =useState([])
 
+  useEffect(()=>{
+    setInterval(()=>{
+      fetch("https://sum-server.100xdevs.com/todos").then( async (res)=>{
+      const jsom = await res.json();
+
+      setTodos(jsom.todos)
+    })
+    },1000)
+  },[])
+
+  return(
+   <div>
       {
-        todos.map((todo) =>(
-          <TodosComponent title={todo.title} description={todo.description}/>
-        )
-         
-        )
+        todos.map((to)=>(
+          <Todos key={to.id} title={to.title} description={to.description}></Todos>
+        ))
       }
-    </div>
-  );
+
+
+   </div>
+  )
 }
 
-
-function TodosComponent({ title ,description}) {
-  return (
-   <div>
-    <h1>{title}</h1>
-    <h3>{description}</h3>
-   </div>
-  );
-};
+function Todos({title,description}){
+  return(
+    <div>
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+    </div>
+  )
+}
 
 export default App;
